df057391a593df57cc50d7730edd12c2334e7ca2
= <=>
2023-03-07 17:36:29 -0600

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input.Manipulations;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Foundation;
+
+if (ApiInformation.IsTypePresent("Windows.UI.Notifications.Management.UserNotificationListener"))
+{
+    // Listener supported!
+}
+ 
+else
+{
+    // Older version of Windows, no Listener
+}
+
+
+namespace testwpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
